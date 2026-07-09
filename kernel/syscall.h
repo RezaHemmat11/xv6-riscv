41585c09401d00f0066b1dcd72e135e76f9cd4a2
RezaHemmat11 <gholamrezahemmat7@gmail.com>
2026-07-10 01:31:25 +0430

    Fixed Test

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -22,3 +22,4 @@
 #define SYS_close  21
 #define SYS_sync   22
 #define SYS_getpinfo 22
+#define SYS_sleep 13
--- user/user.h
+++ user/user.h
@@ -50,3 +50,4 @@ void printf(const char *, ...) __attribute__((format(printf, 1, 2)));
 // umalloc.c
 void *malloc(uint);
 void free(void *);
+int sleep(int);
